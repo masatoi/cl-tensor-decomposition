43b8004e6a4d65755a83b60d6cfdf6ba3b5054ae
Satoshi Imai <satoshi.imai@gmail.com>
2019-02-22 17:22:58 +0900

    mv tests/cltd.lisp -> tests/cl-tensor-decomposition.lisp

---
+++ tests/cl-tensor-decomposition.lisp
@@ -0,0 +1,22 @@
+(defpackage cltd-test
+  (:use :cl
+        :cltd
+        :prove))
+(in-package :cltd-test)
+
+;; NOTE: To run this test file, execute `(asdf:test-system :cl-tensor-decomposition)' in your Lisp.
+
+(plan nil)
+
+(defparameter X-shape '(2 3 4))
+(defparameter X-indices-matrix
+  (make-array '(3 3) :element-type 'fixnum
+              :initial-contents '((0 1 0) ; The row corresponds one datum
+                                  (1 2 3)
+                                  (0 0 1))))
+(defparameter X-value-vector
+  (make-array 3 :element-type 'double-float :initial-contents '(1d0 2d0 3d0)))
+
+(ok (decomposition X-shape X-indices-matrix X-value-vector :n-cycle 100 :R 2 :verbose t))
+
+(finalize)
