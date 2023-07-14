99edcfea0f8bd80b502531bde93043e32f2eee31
Satoshi Imai <satoshi.imai@gmail.com>
2023-07-15 04:04:24 +0900

    Fix simple example and README

--- examples/simple-example.lisp
+++ examples/simple-example.lisp
@@ -13,30 +13,29 @@
                                   (0 0 1))))
 (defparameter X-value-vector
   (make-array number-of-non-zero-elements
-              :element-type 'double-float
-              :initial-contents '(1d0 2d0 3d0)))
+              :element-type 'single-float
+              :initial-contents '(1.0 2.0 3.0)))
 
 ;; Decomposition
 
 (decomposition X-shape X-indices-matrix X-value-vector :n-cycle 10 :R 2 :verbose t)
 
-;; cycle: 1, kl-divergence: 3.8123698367538257d0
-;; cycle: 2, kl-divergence: 7.008589548052008d0
-;; cycle: 3, kl-divergence: 6.417773781756691d0
-;; cycle: 4, kl-divergence: 3.5830575816014383d0
-;; cycle: 5, kl-divergence: 2.7313355001902684d0
-;; cycle: 6, kl-divergence: 1.8856385081647657d0
-;; cycle: 7, kl-divergence: 1.1461000700038138d0
-;; cycle: 8, kl-divergence: 0.7998436391579001d0
-;; cycle: 9, kl-divergence: 0.7485792224097678d0
-;; cycle: 10, kl-divergence: 0.7493554240786242d0
+#|
+cycle: 1, kl-divergence: 12.340305
+cycle: 2, kl-divergence: 6.9702463
+cycle: 3, kl-divergence: 6.4631243
+cycle: 4, kl-divergence: 4.553627
+cycle: 5, kl-divergence: 4.044311
+cycle: 6, kl-divergence: 3.4903245
+cycle: 7, kl-divergence: 2.7311742
+cycle: 8, kl-divergence: 2.5847356
+cycle: 9, kl-divergence: 2.1521056
+cycle: 10, kl-divergence: 1.8600065
 
-;; #(#2A((4.769308790721058d-16 0.6089092386248208d0)
-;;       (0.5846002362657854d0 8.941721411873916d-201))
-;;   #2A((3.697392892947788d-5 1.638528941882213d0)
-;;       (6.457393956786914d-6 0.5461812701566077d0)
-;;       (1.2931746888759577d0 1.912174298594477d-59))
-;;   #2A((1.6699059512771195d-9 0.7517156616482887d0)
-;;       (1.4550066567623938d-8 2.2551469795230017d0)
-;;       (0.0d0 0.0d0)
-;;       (2.645447975546872d0 7.443966374813667d-109)))
+#(#2A((0.6189548 0.16031098) (0.0 0.71152395))
+  #2A((1.6513237 0.017198939) (0.2164403 0.3754391) (0.0 1.2300345))
+  #2A((0.3765476 0.38967007)
+      (2.6936934 6.8288343e-4)
+      (0.0 0.0)
+      (0.0 1.3418932)))
+|#
